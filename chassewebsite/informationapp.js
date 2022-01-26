76d3ef3e7021c49088aa7dc8e7d33796968687ff
andreas swaan <andreasswaan@gmail.com>
2022-01-26 23:43:13 +0100

    loginpage

---
+++ chassewebsite/informationapp.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAFQaTDSNixtdw4oVff9MyeIgmojCKWDDs",
+    authDomain: "chasse-aredenne.firebaseapp.com",
+    projectId: "chasse-aredenne",
+    storageBucket: "chasse-aredenne.appspot.com",
+    messagingSenderId: "1033015324564",
+    appId: "1:1033015324564:web:7b8492577a2c64bd6c2981",
+    measurementId: "G-1R5P1FWKSJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
